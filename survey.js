732cc5edd1cae608c8211b163cd463e82c4bd6a7
YvetteGahamanyi <ygahamanyi26@gmail.com>
2024-06-19 18:11:02 +0200

    init structure

---
+++ survey.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {});
+
+function clearError(errorId) {
+  document.getElementById(errorId).textContent = "";
+}
+
+function validateForm() {
+  let valid = true;
+
+  return valid;
+}
