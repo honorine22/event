db9a56c597b4e92d4827bb0ed7f09d2da247ebcc
Ange Nicole <angenicolemukundwa22@gmail.com>
2024-06-19 20:43:37 +0200

    Feedback is required

--- survey.js
+++ survey.js
@@ -1,21 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {});
 
 function clearError(errorId) {
-  document.getElementById(errorId).textContent = "";
+    document.getElementById(errorId).textContent = "";
 }
 
 function validateForm() {
-  let valid = true;
+    let valid = true;
 
-  const rating = document.getElementById('rating').value;
+    const rating = document.getElementById('rating').value;
 
-  const ratingError = document.getElementById('ratingError');
+    const ratingError = document.getElementById('ratingError');
 
-  ratingError.textContent = '';
+    ratingError.textContent = '';
 
-  if (rating < 1 || rating > 5) {
-    ratingError.textContent = 'Rating is required';
-    valid = false;
-}
-  return valid;
+    const feedbackPattern = /^[a-zA-Z0-9\s]+$/;
+    if (!feedbackPattern.test(feedback)) {
+        feedbackError.textContent = 'Feedback is required.';
+        valid = false;
+    }
+
+    if (rating < 1 || rating > 5) {
+        ratingError.textContent = 'Rating is required';
+        valid = false;
+    }
+    return valid;
 }
